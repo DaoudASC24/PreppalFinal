bce09b665e6bd110649431cdc4fc46e134df98df
Ase-design <folukenny31@gmail.com>
2024-08-14 16:19:33 -0400

    Add files via upload

---
+++ demo Project/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+    document.querySelectorAll('.nav-links a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
